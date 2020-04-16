88822e458253ac4a26fd48d679437607a8a77e11
Real Mohsin <realmohsin@outlook.com>
2020-04-16 11:10:16 -0400

    add materialui plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,13 @@
 module.exports = {
-  plugins: ['gatsby-plugin-react-helmet']
+  plugins: [
+    'gatsby-plugin-react-helmet',
+    {
+      resolve: 'gatsby-plugin-material-ui',
+      options: {
+        stylesProvider: {
+          injectFirst: true
+        }
+      }
+    }
+  ]
 }
